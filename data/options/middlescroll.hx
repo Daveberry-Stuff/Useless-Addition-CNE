30adeafb7738edea9ee3d6e061521fd48c4baf2c
Daveberry <84567826+daveberrys@users.noreply.github.com>
2025-04-05 13:32:47 +0800

    made it work with opponent!! YAYY

--- data/options/middlescroll.hx
+++ data/options/middlescroll.hx
@@ -1,5 +1,8 @@
 function postCreate() {
     for (i in 0...4) {
-        playerStrums.members[i].x -= 322;
+        if (!PlayState.opponentMode)
+            playerStrums.members[i].x -= 322;
+        else
+            cpuStrums.members[i].x += 322;
     }
 }
--- data/options/opponentarrows.hx
+++ data/options/opponentarrows.hx
@@ -1,8 +1,18 @@
 var UAmiddleScroll = FlxG.save.data.UAmiddleScroll;
 
 function postCreate() {
-	cpuStrums.visible = true;
-	if (UAmiddleScroll)
-		for (i in 2...4)
-			cpuStrums.members[i].x += 633;
+	if (!PlayState.opponentMode) {
+		cpuStrums.visible = true;
+		if (UAmiddleScroll)
+			for (i in 2...4)
+				cpuStrums.members[i].x += 633;
+	} else {
+		playerStrums.visible = true;
+		if (UAmiddleScroll) {
+			for (i in 0...4)
+				playerStrums.members[i].x -= 633;
+			for (i in 2...4)
+				playerStrums.members[i].x += 633;
+		}
+	}
 }
